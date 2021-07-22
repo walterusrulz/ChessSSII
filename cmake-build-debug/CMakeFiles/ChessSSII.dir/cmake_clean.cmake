28daccc4f4c6a156baf1cd6a31e124b9ea7a237a
walterusrulz <zona_22@hotmail.com>
2021-07-22 21:19:35 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/ChessSSII.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessSSII.dir/main.cpp.obj"
+  "ChessSSII.exe"
+  "ChessSSII.exe.manifest"
+  "ChessSSII.pdb"
+  "libChessSSII.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessSSII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ChessSSII.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ChessSSII.
+# This may be replaced when dependencies are built.
